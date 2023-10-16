15ba9d9f05fbee0b326401dc48a4928a3133fec6
janakmandavgade <janak2703@gmail.com>
2023-10-16 17:35:32 +0530

    Edited For Vercel

--- server/index.js
+++ server/index.js
@@ -29,13 +29,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors(
-  // {
-  //   origin:["https://deploy-mern-1whq.vercel.app"],
-  //   methods: ["POST","GET"],
-  //   credentials: true
-  // }
-));
+app.use(
+  cors({
+    origin: ["https://socialmediaappmern-api.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
