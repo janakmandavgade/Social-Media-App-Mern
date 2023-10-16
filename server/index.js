2d4391d878042073544568db3323cb220093411b
janakmandavgade <janak2703@gmail.com>
2023-10-16 17:50:22 +0530

    added port again

--- server/index.js
+++ server/index.js
@@ -67,17 +67,17 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
-// const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  // .then(() => {
-  //   app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
   /* ADD DATA ONE TIME */
   // User.insertMany(users);
   // Post.insertMany(posts);
-  // })
+  })
   .catch((error) => console.log(`${error} did not connect`));
